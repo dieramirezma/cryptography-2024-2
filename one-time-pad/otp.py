ac1915815968bfc011a8c08ae308ad405760e8dd
dieramirezma <dieramirezma@unal.edu.co>
2024-12-10 18:48:11 -0500

    refactor: improve code formatting and structure in Caesar, One-Time Pad, Vigenere and Play Fair implementations

--- one-time-pad/otp.py
+++ one-time-pad/otp.py
@@ -3,60 +3,68 @@ import random
 # List of alphabets
 alphabet_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
+
 # Generate random key
 def generateKey(length):
-  return ''.join([random.choice(alphabet_list) for i in range(length)])
+    return "".join([random.choice(alphabet_list) for i in range(length)])
+
 
 # OTP encryption
 def encrypt(plain_text, key):
-  plain_text_indexes = []
-  key_indexes = []
+    plain_text_indexes = []
+    key_indexes = []
 
-  # Get the index of each character of the plain text and key
-  for i in range(len(plain_text)):
-    plain_text_indexes.append(alphabet_list.index(plain_text[i]))
-    key_indexes.append(alphabet_list.index(key[i]))
+    # Get the index of each character of the plain text and key
+    for i in range(len(plain_text)):
+        plain_text_indexes.append(alphabet_list.index(plain_text[i]))
+        key_indexes.append(alphabet_list.index(key[i]))
 
-  # Sum indexes and apply modulo 26
-  cipher_text_indexes = [(a + b) % 26 for a, b in zip(plain_text_indexes, key_indexes)]
+    # Sum indexes and apply modulo 26
+    cipher_text_indexes = [
+        (a + b) % 26 for a, b in zip(plain_text_indexes, key_indexes)
+    ]
 
-  # Get the character from the index
-  return ''.join([alphabet_list[index] for index in cipher_text_indexes])
+    # Get the character from the index
+    return "".join([alphabet_list[index] for index in cipher_text_indexes])
+
 
 # OTP decryption
 def decrypt(cipher_text, key):
-  cipher_text_indexes = []
-  key_indexes = []
+    cipher_text_indexes = []
+    key_indexes = []
 
-  for i in range(len(cipher_text)):
-    cipher_text_indexes.append(alphabet_list.index(cipher_text[i]))
-    key_indexes.append(alphabet_list.index(key[i]))
+    for i in range(len(cipher_text)):
+        cipher_text_indexes.append(alphabet_list.index(cipher_text[i]))
+        key_indexes.append(alphabet_list.index(key[i]))
 
-  # Substrract indexes and apply modulo 26
-  plain_text_indexes = [(a - b) % 26 for a, b in zip(cipher_text_indexes, key_indexes)]
+    # Substrract indexes and apply modulo 26
+    plain_text_indexes = [
+        (a - b) % 26 for a, b in zip(cipher_text_indexes, key_indexes)
+    ]
 
-  return ''.join([alphabet_list[index] for index in plain_text_indexes])
+    return "".join([alphabet_list[index] for index in plain_text_indexes])
 
-if __name__ == '__main__':
-  try:
-    print("One Time Pad Encryption")
-    print("Select an option: \n1. Encrypt\n2. Decrypt")
-    option = int(input("Enter your choice: "))
-    if option == 1:
-      message = input("Enter the message: ").upper().replace(" ", "")
-      print("Message: ", message)
-      key = generateKey(len(message))
-      print("Key: ", key)
-      cipher_text = encrypt(message, key)
-      print("Cipher text: ", cipher_text)
-    elif option == 2:
-      cipher_text = input("Enter the cipher text: ").replace(" ", "")
-      key = input("Enter the key: ").replace(" ", "")
-      plain_text = decrypt(cipher_text, key)
-      print("Plain text: ", plain_text)
-    else:
-      raise ValueError
-  except ValueError:
-    print("Invalid input.")
-  except Exception as e:
-    print("An error occurred: ", e)
+
+if __name__ == "__main__":
+    try:
+        print("One Time Pad Encryption")
+        print("Select an option: \n1. Encrypt\n2. Decrypt")
+        option = int(input("Enter your choice: "))
+        if option == 1:
+            message = input("Enter the message: ").upper().replace(" ", "")
+            print("Message: ", message)
+            key = generateKey(len(message))
+            print("Key: ", key)
+            cipher_text = encrypt(message, key)
+            print("Cipher text: ", cipher_text)
+        elif option == 2:
+            cipher_text = input("Enter the cipher text: ").replace(" ", "")
+            key = input("Enter the key: ").replace(" ", "")
+            plain_text = decrypt(cipher_text, key)
+            print("Plain text: ", plain_text)
+        else:
+            raise ValueError
+    except ValueError:
+        print("Invalid input.")
+    except Exception as e:
+        print("An error occurred: ", e)
--- vigenere/vigenere.py
+++ vigenere/vigenere.py
@@ -2,104 +2,110 @@ alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def getValues(type):
-  message = input(f"Enter the {type} text: ").upper().replace(" ", "")
+    message = input(f"Enter the {type} text: ").upper().replace(" ", "")
 
-  k = input("Enter the key: ").upper().replace(" ", "")
+    k = input("Enter the key: ").upper().replace(" ", "")
 
-  m = int(input("\nEnter the length of substrings: "))
+    m = int(input("\nEnter the length of substrings: "))
 
-  return (message, k, m)
+    return (message, k, m)
+
 
 def createMatrix():
-  matrix = []
+    matrix = []
 
-  for i in range(len(alphabet)):
-    matrix.append(alphabet[i:] + alphabet[:i])
+    for i in range(len(alphabet)):
+        matrix.append(alphabet[i:] + alphabet[:i])
 
-  return matrix
+    return matrix
+
 
 def printMatrix(matrix):
-  for row in matrix:
-    print(row)
+    for row in matrix:
+        print(row)
+
 
 def divideString(string, m):
-  return [string[i:i+m] for i in range(0,len(string), m)]
+    return [string[i : i + m] for i in range(0, len(string), m)]
+
 
 def generateKeyList(key, n):
-  key_list = ''
+    key_list = ""
 
-  for i in range(n):
-    key_list += key[i % len(key)]
+    for i in range(n):
+        key_list += key[i % len(key)]
 
-  return key_list
+    return key_list
+
 
 def encrypt(plain_text, matrix, key):
-  cipher_text = ''
+    cipher_text = ""
 
-  for plain_str, key_str in zip(plain_text, key):
-    division = ""
-    for index in range(5):
-      i = alphabet.index(key_str[index])
-      j = alphabet.index(plain_str[index])
-      division += matrix[i][j]
-    cipher_text += division + " "
+    for plain_str, key_str in zip(plain_text, key):
+        division = ""
+        for index in range(5):
+            i = alphabet.index(key_str[index])
+            j = alphabet.index(plain_str[index])
+            division += matrix[i][j]
+        cipher_text += division + " "
 
-  return cipher_text
+    return cipher_text
+
 
 def decrypt(cipher_text, matrix, key):
-  plain_text = ''
+    plain_text = ""
 
-  for cipher_str, key_str in zip(cipher_text, key):
-    division = ""
-    for index in range(5):
-      i = alphabet.index(key_str[index])
-      j = matrix[i].index(cipher_str[index])
-      division += alphabet[j]
-    plain_text += division + " "
+    for cipher_str, key_str in zip(cipher_text, key):
+        division = ""
+        for index in range(5):
+            i = alphabet.index(key_str[index])
+            j = matrix[i].index(cipher_str[index])
+            division += alphabet[j]
+        plain_text += division + " "
 
-  return plain_text
+    return plain_text
 
-if __name__ == '__main__':
-  try:
-    print("Vigenere's Encryption")
-    print("Select an option: \n1. Encrypt\n2. Decrypt")
-    option = int(input("Enter your choice: "))
-    matrix = createMatrix()
-    if option == 1:
-      plain_text, k, m = getValues('plain')
 
-      M = divideString(plain_text, m)
+if __name__ == "__main__":
+    try:
+        print("Vigenere's Encryption")
+        print("Select an option: \n1. Encrypt\n2. Decrypt")
+        option = int(input("Enter your choice: "))
+        matrix = createMatrix()
+        if option == 1:
+            plain_text, k, m = getValues("plain")
 
-      key_extended = generateKeyList(k, len(plain_text))
-      key = divideString(key_extended, m)
+            M = divideString(plain_text, m)
 
-      cipher_text = encrypt(M, matrix, key)
+            key_extended = generateKeyList(k, len(plain_text))
+            key = divideString(key_extended, m)
 
-      print()
-      print(" ".join(key))
-      print(" ".join(M))
+            cipher_text = encrypt(M, matrix, key)
 
-      print("\nCipher text: ", cipher_text)
-    elif option == 2:
-      cipher_text, k, m = getValues('cipher')
+            print()
+            print(" ".join(key))
+            print(" ".join(M))
 
-      M = divideString(cipher_text, m)
+            print("\nCipher text: ", cipher_text)
+        elif option == 2:
+            cipher_text, k, m = getValues("cipher")
 
-      key_extended = generateKeyList(k, len(cipher_text))
-      key = divideString(key_extended, m)
+            M = divideString(cipher_text, m)
 
-      plain_text = decrypt(M, matrix, key)
+            key_extended = generateKeyList(k, len(cipher_text))
+            key = divideString(key_extended, m)
 
-      print()
+            plain_text = decrypt(M, matrix, key)
 
-      print(" ".join(key))
-      print(" ".join(M))
+            print()
 
-      print("\nPlain text: ", plain_text)
-    else:
-      raise ValueError
-  except ValueError:
-    print("Invalid input.")
-  except Exception as e:
-    print("An error occurred: ", e)
+            print(" ".join(key))
+            print(" ".join(M))
 
+            print("\nPlain text: ", plain_text)
+        else:
+            raise ValueError
+    except ValueError:
+        print("Invalid input.")
+    except Exception as e:
+        print("An error occurred: ", e)
